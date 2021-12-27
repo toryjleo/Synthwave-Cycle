38c0ae0f428f4214ae0ebc0d4427ec0a4bbf352b
Tory <toryjleo@gmail.com>
2021-12-27 16:50:40 -0500

    Updated the terrain to spawn infinitely

--- Assets/Scripts/MovementManager.cs
+++ Assets/Scripts/MovementManager.cs
@@ -17,8 +17,18 @@ public class MovementManager : MonoBehaviour
 
 
 
+
     public event NotifyPlayerPosition playerPositionUpdate; // event
 
+
+    private GameObject[,] groundTiles;
+    private const int GROUND_ARRAY_WIDTH  = 3;
+    private int GROUND_ARRAY_HEIGHT = 3;
+    private Vector3 groundSize;
+    private float groundTileSpawnHeight = -3.12f;
+    private Vector2 currentTileHorizontalMinMax;
+    private Vector2 currentTileVericalMinMax;
+    
 
     private void Awake()
     {
@@ -28,7 +38,7 @@ public class MovementManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        InitializeGround();
 
         bike.EquipGun(InitialPlayerGun);
         //Spawn Cactai 
@@ -50,6 +60,86 @@ public class MovementManager : MonoBehaviour
     {
         ApplyForces();
         UpdateLocations();
+    }
+
+    private void Update()
+    {
+        CheckUpdateGroundTiles();
+    }
+
+    private void InitializeGround() 
+    {
+        groundSize = ground.GetComponent<Renderer>().bounds.size;
+        float groundWidth = groundSize.x;
+        float groundHeight = groundSize.z;
+        groundTiles = new GameObject[GROUND_ARRAY_WIDTH, GROUND_ARRAY_HEIGHT];
+
+        // Initialize tiles at origin
+        for (int i = 0; i < GROUND_ARRAY_HEIGHT; i++) 
+        {
+            for (int j = 0; j < GROUND_ARRAY_WIDTH; j++) 
+            {
+                GameObject goundTile = Instantiate(ground);
+                Vector3 spawnLocation = new Vector3(groundHeight * (i - 1), groundTileSpawnHeight, groundWidth * (j - 1));
+                goundTile.transform.position = spawnLocation;
+                groundTiles[i, j] = goundTile;
+            }
+        }
+        UpdateMiddleTileMinMax();
+    }
+
+    private GameObject GetMiddleTile() 
+    {
+        int middleHeightIdx = GetMiddleTileVerticalIdx();
+        int middleWidthIdx = GetMiddleTileHorizontalIdx();
+        return groundTiles[middleHeightIdx, middleWidthIdx];
+    }
+    
+    private int GetMiddleTileVerticalIdx()
+    {
+        return (GROUND_ARRAY_HEIGHT - 1) / 2;
+    }
+
+    private int GetMiddleTileHorizontalIdx() 
+    {
+        return (GROUND_ARRAY_WIDTH - 1) / 2;
+    }
+
+    private void UpdateMiddleTileMinMax() 
+    {
+        Vector3 currentTileLocation = GetMiddleTile().transform.position;
+        float halfTileWidth = groundSize.x / 2;
+        currentTileHorizontalMinMax = new Vector2(currentTileLocation.x - halfTileWidth, currentTileLocation.x + halfTileWidth);
+        float halfTileHeight = groundSize.z / 2;
+        currentTileVericalMinMax = new Vector2(currentTileLocation.z - halfTileHeight, currentTileLocation.z + halfTileHeight);
+    }
+
+    private void CheckUpdateGroundTiles() 
+    {
+        Vector3 bikePosition = bike.transform.position;
+        float bikeHorizontalPos = bikePosition.x;
+        float bikeVerticalPos = bikePosition.z;
+        if (bikeHorizontalPos > currentTileHorizontalMinMax.y && bikeVerticalPos > currentTileVericalMinMax.y) { MoveGroundTiles(1, 1); } // Upper Right
+        else if (bikeHorizontalPos > currentTileHorizontalMinMax.y && bikeVerticalPos < currentTileVericalMinMax.x) { MoveGroundTiles(1, -1); } // Lower Right
+        else if (bikeHorizontalPos < currentTileHorizontalMinMax.x && bikeVerticalPos > currentTileVericalMinMax.y) { MoveGroundTiles(-1, 1); } // Upper Left
+        else if (bikeHorizontalPos < currentTileHorizontalMinMax.x && bikeVerticalPos < currentTileVericalMinMax.x) { MoveGroundTiles(-1, -1); } // Lower Left
+        else if (bikeHorizontalPos > currentTileHorizontalMinMax.y) { MoveGroundTiles(1, 0); } // Right Quyadrant
+        else if (bikeHorizontalPos < currentTileHorizontalMinMax.x) { MoveGroundTiles(-1, 0); } // Left Quadrant
+        else if (bikeVerticalPos > currentTileVericalMinMax.y) { MoveGroundTiles(0, 1); } // Upper Quadrant
+        else if (bikeVerticalPos < currentTileVericalMinMax.x) { MoveGroundTiles(0, -1); } // Lower Quadrant
+    }
+
+    private void MoveGroundTiles(int xDiff, int yDiff) 
+    {
+        for (int i = 0; i < GROUND_ARRAY_HEIGHT; i++)
+        {
+            for (int j = 0; j < GROUND_ARRAY_WIDTH; j++)
+            {
+                Vector3 newPosition = groundTiles[i, j].transform.position + new Vector3(xDiff * groundSize.x, 0, yDiff * groundSize.z);
+                groundTiles[i, j].transform.position = newPosition;
+            }
+        }
+        UpdateMiddleTileMinMax();
     }
 
     /// <summary>Updates the floor mesh to reflect Player movement.</summary>
