22f75f0aa46806b86ee2370ad8869a50c844d7d6
Tomerd-afk <67992389+Tomerd-afk@users.noreply.github.com>
2022-01-31 21:12:36 -0500

    Variables  Moved, update override Added

--- Assets/Scripts/AI/AiTemplate.cs
+++ Assets/Scripts/AI/AiTemplate.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public abstract class AiTemplate : SelfDespawn
 {
+
+    //Vars for hunting the player 
+    public Vector3 targetVec; //this is the vector to their quarry 
+    public GameObject target;
+
+    //potential inports 
+    public CyborgAnimationStateController animationStateController;
+    public Rigidbody rb;
+    public Gun myGun;
+
+    //stats used in construction 
+    public Health hp;
+    public float score;
+    public float StartingHP;
+    public float maxSpeed;
+    public float maxForce;
+    public float attackRange; //TODO: This will be set when creating different inherited classes for Monobehavior; 
+    public bool alive;
+
+
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    public override void Update()
     {
-        
+        base.Update();
     }
 }
--- Assets/Scripts/AI/EnemyAI.cs
+++ Assets/Scripts/AI/EnemyAI.cs
@@ -4,23 +4,6 @@ using UnityEngine;
 
 public class EnemyAI : AiTemplate
 {
-    //Vars for hunting the player 
-    public Vector3 targetVec; //this is the vector to their quarry 
-    public GameObject target;
-
-    //potential inports 
-    public CyborgAnimationStateController animationStateController;
-    public Rigidbody rb;
-    public Gun myGun;
-
-    //stats used in construction 
-    public Health hp;
-    public float score;
-    public float StartingHP;
-    public float maxSpeed;
-    public float maxForce;
-    public float attackRange; //TODO: This will be set when creating different inherited classes for Monobehavior; 
-    public bool alive;
 
 
     public float hitpoints
