29f82cd1a3a116445ce099b85e9be18a240e40a1
Tory <toryjleo@gmail.com>
2022-02-20 18:38:11 -0500

    Cleaned up script

--- Assets/Scripts/AI/CyborgAnimationStateController.cs
+++ Assets/Scripts/AI/CyborgAnimationStateController.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>Class <c>CyborgAnimationStateController</c> A Unity Component controls the child animator component.</summary>
+/// Requires child object to have an Animator component
 public class CyborgAnimationStateController : MonoBehaviour
 {
     Animator animator;
@@ -26,6 +28,11 @@ public class CyborgAnimationStateController : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
+        Init();
+    }
+
+    private void Init() 
+    {
         animator = GetComponentInChildren<Animator>();
         if (animator == null)
         {
@@ -33,19 +40,19 @@ public class CyborgAnimationStateController : MonoBehaviour
         }
         else
         {
-            speedHash       = Animator.StringToHash("Speed");
-            shootTriple     = Animator.StringToHash("ShootTriple");
-            shootSingle     = Animator.StringToHash("ShootSingle");
+            speedHash = Animator.StringToHash("Speed");
+            shootTriple = Animator.StringToHash("ShootTriple");
+            shootSingle = Animator.StringToHash("ShootSingle");
             aimWhileWalking = Animator.StringToHash("IsAimingWhileWalking");
-            deathAHash      = Animator.StringToHash("Death_A");
-            deathBHash      = Animator.StringToHash("Death_B");
-            deathCHash      = Animator.StringToHash("Death_C");
-            isAliveHash     = Animator.StringToHash("isAlive");
+            deathAHash = Animator.StringToHash("Death_A");
+            deathBHash = Animator.StringToHash("Death_B");
+            deathCHash = Animator.StringToHash("Death_C");
+            isAliveHash = Animator.StringToHash("isAlive");
         }
     }
 
     protected private void Update()
-    {
+    {/*
         if (Input.GetKeyDown(KeyCode.Alpha7)) 
         {
             ShootSingle();
@@ -71,7 +78,7 @@ public class CyborgAnimationStateController : MonoBehaviour
             AimWhileWalking(false);
         }
 
-        SetSpeed(speed);
+        SetSpeed(speed);*/
     }
 
     /// <summary>Updates the animation controller with the current speed. Should be called every frame when awake.</summary>
@@ -90,21 +97,32 @@ public class CyborgAnimationStateController : MonoBehaviour
 
     #region Shooting
 
+    /// <summary>Will return true if the animator is in the idle blend tree.</summary>
+    /// <returns>True if the animator is in the idle blend tree.</returns>
     public bool IsIdle() 
     {
         return animator.GetCurrentAnimatorStateInfo(0).IsName(COMBAT_IDLE);
     }
 
+    /// <summary>Will return true if the animator is in the walking blend tree.</summary>
+    /// <returns>True if the animator is in the walking blend tree.</returns>
     public bool IsWalking() 
     {
         return animator.GetCurrentAnimatorStateInfo(0).IsName(WALKING);
     }
 
+    /// <summary>
+    /// Sets a boolean to trigger an aiming while walking animation when moving
+    /// </summary>
+    /// <param name="useWalkAim">When set to true, will use the aiming whle walking animation when moving</param>
     public void AimWhileWalking(bool useWalkAim)
     {
         animator.SetBool(aimWhileWalking, useWalkAim);
     }
 
+    /// <summary>
+    /// This method plays the triple shot animation when the character is at idle
+    /// </summary>
     public void ShootTriple()
     {
         if (IsIdle())
@@ -117,6 +135,9 @@ public class CyborgAnimationStateController : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// This method plays the single shot animation when the character is at idle
+    /// </summary>
     public void ShootSingle()
     {
         if (IsIdle())
