1d13a92c24a7a9323667a6c89df04cd90a985ac3
Tory <toryjleo@gmail.com>
2022-09-18 11:12:02 -0400

    Added a heal event

--- Assets/Scripts/Player/Health.cs
+++ Assets/Scripts/Player/Health.cs
@@ -2,10 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public delegate void NotifyHeal();
+
 /// <summary>Class <c>Health</c> A Unity Component which tracks health.</summary>
 public class Health : MonoBehaviour
 {
     private float _hitPoints;
+    public NotifyHeal healEvent;
+
 
 
     public float HitPoints
@@ -34,6 +39,9 @@ public class Health : MonoBehaviour
     /// <param name="hp">The number of points to add to _hitPoints.</param>
     public void Heal(float hp) 
     {
+        // Notify effects that this is healing
+        healEvent?.Invoke();
+
         // Ensure we do not overflow
         if (_hitPoints+hp < _hitPoints) 
         {
