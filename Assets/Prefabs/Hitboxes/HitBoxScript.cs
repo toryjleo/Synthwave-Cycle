2b2643c473028fef942284220eb4ef14b21faf00
Tomerd-afk <67992389+Tomerd-afk@users.noreply.github.com>
2022-11-05 16:13:46 -0400

    Added comments

--- Assets/Prefabs/Hitboxes/HitBoxScript.cs
+++ Assets/Prefabs/Hitboxes/HitBoxScript.cs
@@ -1,14 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// This class is used to activate and deactivate hitboxes for weapons and ramming zones as well as other abilities 
+/// </summary>
 public class HitBoxScript : MonoBehaviour
 {
     /// <summary>
     /// This script searchers for all colliders in child of the prefab it is attached to. It will then activate and deactivate all of them when it's method is called. 
     /// </summary>
-
-
     // This is the list that stores all the hitboxes. 
     private List<Collider> C;
 
@@ -30,7 +30,7 @@ public class HitBoxScript : MonoBehaviour
     /// <summary>
     /// This is a submethod that takes in each collider as a parameter and activates them
     /// </summary>
-    /// <param name="c"></param>
+    /// <param name="c">This is the one of the coliders in the child of the prefab passed in throught the Activate Method</param>
     void ActivateHitbox(Collider c)
     {
         c.enabled = true;
@@ -48,7 +48,7 @@ public class HitBoxScript : MonoBehaviour
     /// <summary>
     /// This is a submethod that tackes in each  collider as a parameter and deactivates them
     /// </summary>
-    /// <param name="c"></param>
+    /// <param name="c">This is the one of the coliders in the child of the prefab passed in throught the Activate Method</param>
     void DeActivateHitbox(Collider c)
     {
         c.enabled = false;
