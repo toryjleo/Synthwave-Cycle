ecc6d89eeb61be8926f7a714b433b96404f5b22b
Tomerd-afk <67992389+Tomerd-afk@users.noreply.github.com>
2022-11-14 20:12:18 -0500

    Added comments

--- Assets/Scripts/Guns/Weapon.cs
+++ Assets/Scripts/Guns/Weapon.cs
@@ -2,15 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// This class is the base abstract class that will be used for all weapons and guns. 
+/// In the PR please list any aditional features you want for this class
+/// </summary>
 public abstract class Weapon : MonoBehaviour
 {
 
-
+    /// <summary>
+    /// The Awake Function, initializes the Weapon 
+    /// </summary>
     protected virtual void Awake()
     {
         Init();
     }
-
+    /// <summary>
+    /// Used to deInit the weapon 
+    /// </summary>
     protected virtual void OnDestroy()
     {
         DeInit();
@@ -21,7 +29,7 @@ public abstract class Weapon : MonoBehaviour
     /// <summary>Basically a destructor. Calls bulletPool.DeInit().</summary>
     public virtual void DeInit()
     {
-
+       //TODO: add more featuers? Thoughts
     }
 
     //TODO: Add Code for Weapons and items being able to be picked up here! 
