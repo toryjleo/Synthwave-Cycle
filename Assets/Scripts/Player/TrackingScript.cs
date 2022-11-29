e2c3b37a3bef0bd190c459c23a8e0cc5b268403c
Tomerd-afk <67992389+Tomerd-afk@users.noreply.github.com>
2022-11-29 17:52:38 -0500

    Final Edits

--- Assets/Scripts/Player/TrackingScript.cs
+++ Assets/Scripts/Player/TrackingScript.cs
@@ -13,7 +13,7 @@ public class TrackingScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
+        player = GameObject.FindGameObjectWithTag("TrackThis");
 
         TPoints = GameObject.FindGameObjectsWithTag("TrackerChild");
 
