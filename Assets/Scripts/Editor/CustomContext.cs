cf094d75610dfb65f87c7a0b44a8c74c7592ed44
Tory <toryjleo@gmail.com>
2023-04-02 14:24:40 -0400

    Added tool to create a PBR HLSL shader

--- Assets/Scripts/Editor/CustomContext.cs
+++ Assets/Scripts/Editor/CustomContext.cs
@@ -12,6 +12,7 @@ public class CustomContext
 
     private const string CUSTOM_LIT_SHADER_DIR = "Assets/Scripts/Editor/";
     private const string CUSTOM_LIT_SHADER_NAME = "CustomLit.shader";
+    private const string PHYSICALLY_BASED_NAME = "PBR.shader";
 
     /// <summary>
     /// 
@@ -24,6 +25,20 @@ public class CustomContext
         reader.Close();
         ProjectWindowUtil.CreateAssetWithContent(
         CUSTOM_LIT_SHADER_NAME,
+        shaderRaw);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [MenuItem("Assets/Create/Shader/Universal Render Pipeline/PBR_HLSL")]
+    private static void CreatePBRShader()
+    {
+        StreamReader reader = new StreamReader(CUSTOM_LIT_SHADER_DIR + PHYSICALLY_BASED_NAME);
+        string shaderRaw = reader.ReadToEnd();
+        reader.Close();
+        ProjectWindowUtil.CreateAssetWithContent(
+        PHYSICALLY_BASED_NAME,
         shaderRaw);
     }
 }
