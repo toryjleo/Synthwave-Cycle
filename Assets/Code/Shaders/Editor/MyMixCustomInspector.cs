18afcc7c6a556ae924a9a8925e35ae30c95f85c7
toryjleo <toryjleo@gmail.com>
2023-11-21 15:58:08 -0500

    Now hiding material properties behind dropdown

--- Assets/Code/Shaders/Editor/MyMixCustomInspector.cs
+++ Assets/Code/Shaders/Editor/MyMixCustomInspector.cs
@@ -1,9 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class MyMixCustomInspector : MyCustomShaderInspector
 {
+    protected bool foldedOut = true;
+    string[] PropertyNames = { "_ColorMap1", "_ColorTint1", "_NormalMap1", "_NormalStrength1", "_MetalnessMap1", 
+        "_MetalnessStrength1", "_SpecularMap1", "_SpecularTint1", "_SmoothnessMask1", "_Smoothness1", "_EmissionMap1",
+    "_EmissionTint1", "_ClearCoatMask1", "_ClearCoatStrength1", "_ClearCoatSmoothnessMask1", "_ClearCoatSmoothness1",
+    "_ParallaxMap1", "_ParallaxStrength1"};
+    public override void OnGUI(MaterialEditor materialEditor, MaterialProperty[] properties)
+    {
+        base.OnGUI(materialEditor, properties);
+
+        foldedOut = EditorGUILayout.BeginFoldoutHeaderGroup(foldedOut, "Properties for Material 1");
+        if (foldedOut)
+        { 
+            
+            foreach (string Reference in PropertyNames)
+            {
+                MaterialProperty SunReference = ShaderGUI.FindProperty(Reference, properties);
+                materialEditor.ShaderProperty(SunReference, SunReference.displayName);
+
+                //EditorGUILayout.Separator();
+            }
+        }
+        EditorGUILayout.EndFoldoutHeaderGroup();
+    }
     public override void AssignNewShaderToMaterial(Material material, Shader oldShader, Shader newShader)
     {
         base.AssignNewShaderToMaterial(material, oldShader, newShader);
