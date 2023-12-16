2c72d518b5a5e7232b243823ff8d0f60a0ee26cc
LiamMillerSE <LiamMillerSE@gmail.com>
2023-12-16 13:18:42 -0500

    Implemented Game Level and Level Manager
    Created level for InTheirFinalMoments

---
+++ Assets/Scripts/GameManagement/GameLevel.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLevel : MonoBehaviour
+{
+    [SerializeField]
+    public WaveSequence WaveSequence;
+    [SerializeField]
+    public Material GroundMat;
+}
