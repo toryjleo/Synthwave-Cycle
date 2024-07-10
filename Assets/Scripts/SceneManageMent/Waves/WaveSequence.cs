90acaa66f4e79fa871318b67944f135a556435bc
Shadowmage <baconispowerful@gmail.com>
2024-07-10 11:40:29 -0400

    Wave Sequence song name public

--- Assets/Scripts/SceneManageMent/Waves/WaveSequence.cs
+++ Assets/Scripts/SceneManageMent/Waves/WaveSequence.cs
@@ -10,63 +10,65 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Wave/WaveSequence", fileName = "New Wave Sequence")]
 public class WaveSequence : ScriptableObject, IResettable
 {
-    private int previousWave = -1;
-    private int currentWave = 0;
-    [SerializeField]
-    public List<Wave> sequence;
-    internal SquadSpawner spawner;
+  private int previousWave = -1;
+  private int currentWave = 0;
+  [SerializeField]
+  public List<Wave> sequence;
+  internal SquadSpawner spawner;
+  [SerializeField]
+  public string songName;
 
-    public AudioClip GetCurrentTrackVariation()
-    {
-        return sequence[currentWave].GetTrackVariation();
-    }
-    /// <summary>
-    /// Updates the wave and spawns in wave according to danger level
-    /// </summary>
-    public void SpawnNewWave()
-    {
-        UpdateCurrentWave();
-        spawner.SpawnWave(sequence[currentWave].GetWaveInfo());
-        previousWave = currentWave;
-    }
+  public AudioClip GetCurrentTrackVariation()
+  {
+    return sequence[currentWave].GetTrackVariation();
+  }
+  /// <summary>
+  /// Updates the wave and spawns in wave according to danger level
+  /// </summary>
+  public void SpawnNewWave()
+  {
+    UpdateCurrentWave();
+    spawner.SpawnWave(sequence[currentWave].GetWaveInfo());
+    previousWave = currentWave;
+  }
 
-    /// <summary>
-    /// Sets the currentWave number based on Danger Level
-    /// </summary>
-    internal void UpdateCurrentWave()
+  /// <summary>
+  /// Sets the currentWave number based on Danger Level
+  /// </summary>
+  internal void UpdateCurrentWave()
+  {
+    for (int index = sequence.Count - 1; index >= 0; index--)
     {
-        for (int index = sequence.Count - 1; index >= 0 ; index--)
-        {
-            // Iterate backwards and spawn in the waves for the highest danger level
-            if (sequence[index].IsOverThreshold())
-            {
-                currentWave = index;
-                //Log the wave + 1 because the index starts at 0, but the tracks start at 1
-                Debug.Log("Current Wave: " + (currentWave + 1) + "/" + (sequence.Count) + "\nDanger Level: " + DLevel.Instance.GetDangerLevel());
-                break;
-            }
-        }
+      // Iterate backwards and spawn in the waves for the highest danger level
+      if (sequence[index].IsOverThreshold())
+      {
+        currentWave = index;
+        //Log the wave + 1 because the index starts at 0, but the tracks start at 1
+        Debug.Log("Current Wave: " + (currentWave + 1) + "/" + (sequence.Count) + "\nDanger Level: " + DLevel.Instance.GetDangerLevel());
+        break;
+      }
     }
+  }
 
-    internal void Init(SquadSpawner squadSpawner)
-    {
-        spawner = squadSpawner;
-        currentWave = 0;
-        previousWave = -1;
-    }
-    /// <summary>
-    /// Gets the universal time that the next wave will spawn at,
-    /// according to the waves per loop and loop duration
-    /// </summary>
-    internal double GetNextWaveTime(double currentTime)
-    {
-        AudioClip clipToPlay = sequence[currentWave].GetTrackVariation();
-        double duration = (double)clipToPlay.samples / clipToPlay.frequency;
-        return currentTime + (duration / sequence[currentWave].wavesInTrack);
-    }
+  internal void Init(SquadSpawner squadSpawner)
+  {
+    spawner = squadSpawner;
+    currentWave = 0;
+    previousWave = -1;
+  }
+  /// <summary>
+  /// Gets the universal time that the next wave will spawn at,
+  /// according to the waves per loop and loop duration
+  /// </summary>
+  internal double GetNextWaveTime(double currentTime)
+  {
+    AudioClip clipToPlay = sequence[currentWave].GetTrackVariation();
+    double duration = (double)clipToPlay.samples / clipToPlay.frequency;
+    return currentTime + (duration / sequence[currentWave].wavesInTrack);
+  }
 
-    public void ResetGameObject()
-    {
-        currentWave = 0;
-    }
+  public void ResetGameObject()
+  {
+    currentWave = 0;
+  }
 }
