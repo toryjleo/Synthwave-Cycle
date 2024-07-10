9c7f4dcd55cab8fa98f4166fe34346bcdaa4af99
toryjleo <toryjleo@gmail.com>
2024-07-10 15:24:57 -0400

    Renamed trashbin folder, added a new scene that takes user input and represents it with lines

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    #region InputManagerStrings
+    private const string HORIZONTAL = "Horizontal";
+    private const string VERTICAL = "Vertical";
+    #endregion
+
+    private Vector3 start_position = new Vector3(0, 1, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = start_position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Note: using vertical axis to represent +x and horizontal axis to represent -z
+        float horizontalAxis = Input.GetAxis(HORIZONTAL);
+        float verticalAxis = Input.GetAxis(VERTICAL);
+
+        Vector3 desiredDirection = new Vector3(verticalAxis, 0, -horizontalAxis);
+        Vector3 desiredDirectionNormalized = Vector3.Normalize(desiredDirection);
+        float magnitude = Mathf.Clamp(desiredDirection.magnitude, 0, 1);
+
+
+        Debug.DrawLine(transform.position, transform.position + transform.forward, Color.red);
+        Debug.DrawLine(transform.position, transform.position + (desiredDirectionNormalized * magnitude), Color.magenta);
+
+
+    }
+}
