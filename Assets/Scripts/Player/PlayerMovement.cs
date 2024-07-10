bc86bbce8b0cd5dbe09dd5cb507b3b99b3d47cf0
toryjleo <toryjleo@gmail.com>
2024-07-10 15:38:22 -0400

    Player now has basic movement in direction specified by input

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -9,7 +9,10 @@ public class PlayerMovement : MonoBehaviour
     private const string VERTICAL = "Vertical";
     #endregion
 
+    public Rigidbody rigidBody;
+
     private Vector3 start_position = new Vector3(0, 1, 0);
+    private Vector3 inputDirection = Vector3.zero;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +22,16 @@ public class PlayerMovement : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        inputDirection = GetInputDir();
+
+    }
+    private void FixedUpdate()
+    {
+        rigidBody.AddForce(inputDirection);
+    }
+
+    private Vector3 GetInputDir() 
     {
         // Note: using vertical axis to represent +x and horizontal axis to represent -z
         float horizontalAxis = Input.GetAxis(HORIZONTAL);
@@ -32,6 +45,6 @@ public class PlayerMovement : MonoBehaviour
         Debug.DrawLine(transform.position, transform.position + transform.forward, Color.red);
         Debug.DrawLine(transform.position, transform.position + (desiredDirectionNormalized * magnitude), Color.magenta);
 
-
+        return desiredDirectionNormalized * magnitude;
     }
 }
