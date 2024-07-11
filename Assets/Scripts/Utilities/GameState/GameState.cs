97007fe557ce761fc0a75b77c319b8e2736c4ee6
Shadowmage <baconispowerful@gmail.com>
2024-07-11 10:28:36 -0400

    Added reset state trigger to gameplayPaused state

--- Assets/Scripts/Utilities/GameState/GameState.cs
+++ Assets/Scripts/Utilities/GameState/GameState.cs
@@ -5,124 +5,129 @@ public delegate void StateChangeHandler();
 
 public abstract class GameState
 {
-    public event StateChangeHandler notifyListenersEnter;
-    public event StateChangeHandler notifyListenersExit;
+  public event StateChangeHandler notifyListenersEnter;
+  public event StateChangeHandler notifyListenersExit;
 
-    public abstract GameState HandleTrigger(StateTrigger trigger);
+  public abstract GameState HandleTrigger(StateTrigger trigger);
 
-    public void Enter()
-    {
-        notifyListenersEnter?.Invoke();
-    }
+  public void Enter()
+  {
+    notifyListenersEnter?.Invoke();
+  }
 
-    public void Exit()
-    {
-        notifyListenersExit?.Invoke();
-    }
+  public void Exit()
+  {
+    notifyListenersExit?.Invoke();
+  }
 }
 
 public class Loading : GameState
 {
-    public override GameState HandleTrigger(StateTrigger trigger)
+  public override GameState HandleTrigger(StateTrigger trigger)
+  {
+    if (trigger == StateTrigger.LoadingComplete)
     {
-        if (trigger == StateTrigger.LoadingComplete) 
-        {
-            Exit();
-            return GameStateController.gamesStartPaused;
-        }
-        return null;
+      Exit();
+      return GameStateController.gamesStartPaused;
     }
+    return null;
+  }
 }
 
 public class GameStartPaused : GameState
 {
-    public override GameState HandleTrigger(StateTrigger trigger)
+  public override GameState HandleTrigger(StateTrigger trigger)
+  {
+    if (trigger == StateTrigger.StartGame)
     {
-        if (trigger == StateTrigger.StartGame) 
-        {
-            Exit();
-            return GameStateController.playing;
-        }
-        return null;
+      Exit();
+      return GameStateController.playing;
     }
+    return null;
+  }
 }
 
 public class GamePlayPaused : GameState
 {
-    public override GameState HandleTrigger(StateTrigger trigger)
+  public override GameState HandleTrigger(StateTrigger trigger)
+  {
+    if (trigger == StateTrigger.StartGame)
     {
-        if (trigger == StateTrigger.StartGame) 
-        {
-            Exit();
-            return GameStateController.playing;
-        }
-        return null;
+      Exit();
+      return GameStateController.playing;
     }
+    if (trigger == StateTrigger.Reset)
+    {
+      Exit();
+      return GameStateController.resetting;
+    }
+    return null;
+  }
 }
 
 public class Playing : GameState
 {
-    public override GameState HandleTrigger(StateTrigger trigger)
+  public override GameState HandleTrigger(StateTrigger trigger)
+  {
+    if (trigger == StateTrigger.StartGame)
     {
-        if (trigger == StateTrigger.StartGame) 
-        {
-            Exit();
-            return GameStateController.gamePlayPaused;
-        }
-        else if (trigger == StateTrigger.ZeroHP) 
-        {
-            Exit();
-            return GameStateController.playerDead;
-        }
-        else if (trigger == StateTrigger.LevelComplete) 
-        {
-            Exit();
-            return GameStateController.levelComplete;
-        }
-        return null;
+      Exit();
+      return GameStateController.gamePlayPaused;
     }
+    else if (trigger == StateTrigger.ZeroHP)
+    {
+      Exit();
+      return GameStateController.playerDead;
+    }
+    else if (trigger == StateTrigger.LevelComplete)
+    {
+      Exit();
+      return GameStateController.levelComplete;
+    }
+    return null;
+  }
 }
 
 public class PlayerDead : GameState
 {
-    public override GameState HandleTrigger(StateTrigger trigger)   
+  public override GameState HandleTrigger(StateTrigger trigger)
+  {
+    if (trigger == StateTrigger.Reset)
     {
-        if (trigger == StateTrigger.Reset) 
-        {
-            Exit();
-            return GameStateController.resetting;
-        }
-        return null;
+      Exit();
+      return GameStateController.resetting;
     }
+    return null;
+  }
 }
 
 public class Resetting : GameState
 {
-    public override GameState HandleTrigger(StateTrigger trigger)
+  public override GameState HandleTrigger(StateTrigger trigger)
+  {
+    if (trigger == StateTrigger.LoadingComplete)
     {
-        if (trigger == StateTrigger.LoadingComplete) 
-        {
-            Exit();
-            return GameStateController.gamesStartPaused;
-        }
-        return null;
+      Exit();
+      return GameStateController.gamesStartPaused;
     }
+    return null;
+  }
 }
 
 public class LevelComplete : GameState
 {
-    public override GameState HandleTrigger(StateTrigger trigger)
+  public override GameState HandleTrigger(StateTrigger trigger)
+  {
+    if (trigger == StateTrigger.Reset)
     {
-        if(trigger == StateTrigger.Reset) 
-        {
-            Exit();
-            return GameStateController.resetting;
-        }
-        else if (trigger == StateTrigger.TransitionFromLevel) 
-        {
-            Exit();
-            return GameStateController.loading;
-        }
-        return null;
+      Exit();
+      return GameStateController.resetting;
     }
+    else if (trigger == StateTrigger.TransitionFromLevel)
+    {
+      Exit();
+      return GameStateController.loading;
+    }
+    return null;
+  }
 }
