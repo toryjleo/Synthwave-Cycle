6849c0ced16aeef6f4ac9635ba1b0d474dc8d4dd
toryjleo <toryjleo@gmail.com>
2024-07-15 17:44:01 -0400

    Experimenting with rational functions

--- Assets/Scripts/Player/MotionFunctions/MotionFunctions.cs
+++ Assets/Scripts/Player/MotionFunctions/MotionFunctions.cs
@@ -30,3 +30,65 @@ public class LinearVelocity : MotionFunctions
         return y / 5.0f;
     }
 }
+
+public class Rational1 : MotionFunctions
+{
+    public float Acceleration(float t)
+    {
+        // (1/x^2)
+        if (t == 0) 
+        {
+            return 1;
+        }
+        else 
+        {
+            return 1 / (t * t);
+        }
+    }
+
+    public float GettFromVelocity(float y)
+    {
+        // Check in morning
+        if (y == 0)
+        {
+            return 0;
+        }
+        else 
+        {
+            return 1 / (-y + 1);
+        }
+    }
+
+    public float Velocity(float t)
+    {
+        // (1/-x) + 1
+        if (t == 0) 
+        {
+            return 0;
+        }
+        else
+        {
+            return (1 / -t) + 1;
+        }
+
+    }
+}
+
+public class SmoothArrive1 : MotionFunctions
+{
+    public float Acceleration(float t)
+    {
+        return -3*(t*t) + 2*t + 1;
+    }
+
+    public float GettFromVelocity(float y)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public float Velocity(float t)
+    {
+        //return t * (1 - t) + (1 - ((1 - t) * (1 - t))) * t;
+        return -(t * t * t) + (t * t) + t;
+    }
+}
