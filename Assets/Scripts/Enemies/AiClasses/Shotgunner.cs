ba4b36b9378f242c1b478fbeec70470880ca63bb
LiamMillerSE <LiamMillerSE@gmail.com>
2024-07-23 16:31:27 -0400

    Enemy implementation commit

--- Assets/Scripts/Enemies/AiClasses/Shotgunner.cs
+++ Assets/Scripts/Enemies/AiClasses/Shotgunner.cs
@@ -6,41 +6,6 @@ public class Shotgunner : InfantryAI
 {
     public GameObject muzzleLocation; // Empty GameObject set to the location of the barrel
 
-    public override void Init()
-    {
-        alive = true;
-
-        hp = GetComponentInChildren<Health>();
-        rb = GetComponent<Rigidbody>();
-        animationStateController = GetComponent<CyborgAnimationStateController>();
-        this.Despawn += op_ProcessCompleted;
-        hp.Init(StartingHP);
-
-        #region Error Checkers
-
-        if (animationStateController == null)
-        {
-            Debug.LogError("This object needs a CyborgAnimationStateController component");
-        }
-        if (rb == null)
-        {
-            Debug.LogError("This object needs a rigidBody component");
-        }
-        if (hp == null)
-        {
-            Debug.LogError("This object needs a health component");
-        }
-        #endregion
-    }
-
-
-
-    //stats used in construction
-
-    public float hitpoints
-    {
-        get => hp.HitPoints;
-    }
 
     void Awake()
     {
