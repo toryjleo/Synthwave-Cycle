2d4de44eda1e1612cf3de4c5b5096011557b62b0
toryjleo <toryjleo@gmail.com>
2024-08-20 11:10:26 -0400

    Updated formatting

--- Assets/Code/Scripts/Player/BoundsChecker.cs
+++ Assets/Code/Scripts/Player/BoundsChecker.cs
@@ -6,214 +6,214 @@ public delegate void TimerEventHandler(bool timerIsOn);
 
 public class BoundsChecker : MonoBehaviour
 {
-  /// <summary>
-  /// Reference to the transmissionArea
-  /// </summary>
-  private TransmissionArea transmissionArea;
+    /// <summary>
+    /// Reference to the transmissionArea
+    /// </summary>
+    private TransmissionArea transmissionArea;
 
-  private const float MAX_TIME = 10.0f;
+    private const float MAX_TIME = 10.0f;
 
-  private Timer timer;
+    private Timer timer;
 
 
 
-  private class Timer
-  {
-    private float timeElapsed = 0;
-
-    private float maxTime = 10.0f;
-
-    private bool timerOn = false;
-
-    public TimerEventHandler NotifyTimerEvent;
-    public TimerEventHandler NotifyTimerComplete;
-
-    public bool HasStarted
+    private class Timer
     {
-      get => timerOn;
+        private float timeElapsed = 0;
+
+        private float maxTime = 10.0f;
+
+        private bool timerOn = false;
+
+        public TimerEventHandler NotifyTimerEvent;
+        public TimerEventHandler NotifyTimerComplete;
+
+        public bool HasStarted
+        {
+            get => timerOn;
+        }
+
+        public float TimeLeft
+        {
+            get { return maxTime - timeElapsed; }
+        }
+
+        public float TimePercentage
+        {
+            get { return timeElapsed / maxTime; }
+        }
+
+        public Timer(float maxTime)
+        {
+            this.maxTime = maxTime;
+            this.timeElapsed = 0;
+            this.timerOn = false;
+        }
+
+        public void Tick(float deltaTime)
+        {
+            if (timerOn)
+            {
+                timeElapsed += deltaTime;
+                Debug.Log("timer at " + timeElapsed + " seconds");
+
+                if (timeElapsed > maxTime)
+                {
+                    NotifyTimerComplete?.Invoke(false);
+                    TimerReset();
+                }
+            }
+        }
+
+        public void TimerStart()
+        {
+            if (GameStateController.CanRunGameplay)
+            {
+                timerOn = true;
+                NotifyTimerEvent?.Invoke(true);
+            }
+        }
+
+        public void TimerReset()
+        {
+            timeElapsed = 0;
+            timerOn = false;
+            NotifyTimerEvent?.Invoke(false);
+        }
     }
 
+    /// <summary>
+    /// Returns true if the player has made it outside the playable bounds
+    /// </summary>
+    private bool PlayerIsOutsideBounds
+    {
+        get
+        {
+            if (transmissionArea == null)
+            {
+                Debug.LogError("Trying to find if player outside bounds when there is no TransmissionArea found");
+                return true;
+            }
+
+            float distanceSqrToTransmissionAreaCenter = (transform.position - transmissionArea.transform.position).sqrMagnitude;
+
+            return distanceSqrToTransmissionAreaCenter > transmissionArea.MaxBoundsFromTransmissionAreaSqr;
+        }
+    }
+
+    /// <summary>
+    /// Returns how much time is left on the timer
+    /// </summary>
     public float TimeLeft
     {
-      get { return maxTime - timeElapsed; }
+        get
+        {
+            if (timer == null) { return -1; }
+            else { return timer.TimeLeft; }
+        }
     }
 
+    /// <summary>
+    /// Rising Percentage until timer is completed
+    /// </summary>
     public float TimePercentage
     {
-      get { return timeElapsed / maxTime; }
-    }
-
-    public Timer(float maxTime)
-    {
-      this.maxTime = maxTime;
-      this.timeElapsed = 0;
-      this.timerOn = false;
-    }
-
-    public void Tick(float deltaTime)
-    {
-      if (timerOn)
-      {
-        timeElapsed += deltaTime;
-        Debug.Log("timer at " + timeElapsed + " seconds");
-
-        if (timeElapsed > maxTime)
+        get
         {
-        NotifyTimerComplete?.Invoke(false);
-          TimerReset();
+            if (timer == null) { return -1; }
+            else { return timer.TimePercentage; }
         }
-      }
     }
 
-    public void TimerStart()
+    /// <summary>
+    /// Notifies the listeners that a timer event has triggered.
+    /// Timer events trigger when the player goes out of bounds or enters the bounds again.
+    /// </summary>
+    public TimerEventHandler NotifyTimerEvent
     {
-      if (GameStateController.CanRunGameplay)
-      {
-        timerOn = true;
-        NotifyTimerEvent?.Invoke(true);
-      }
+        get
+        {
+            if (timer == null)
+            {
+                Debug.LogError("Hooking up an event to a null object");
+                return null;
+            }
+            else
+            {
+                return timer.NotifyTimerEvent;
+            }
+        }
+        set { timer.NotifyTimerEvent = value; }
     }
 
-    public void TimerReset()
+    // Start is called before the first frame update
+    void Awake()
     {
-      timeElapsed = 0;
-      timerOn = false;
-      NotifyTimerEvent?.Invoke(false);
-    }
-  }
+        // Find TransmissionArea, disable if not found
+        if (!CanFindTransmissionArea())
+        {
+            this.gameObject.SetActive(false);
+            Debug.LogWarning("BoundsChecker did not find a TransmissionArea in scene. Disabling bounds");
+        }
 
-  /// <summary>
-  /// Returns true if the player has made it outside the playable bounds
-  /// </summary>
-  private bool PlayerIsOutsideBounds
-  {
-    get
+        InitTimer();
+    }
+
+    // Update is called once per frame
+    void Update()
     {
-      if (transmissionArea == null)
-      {
-        Debug.LogError("Trying to find if player outside bounds when there is no TransmissionArea found");
-        return true;
-      }
-
-      float distanceSqrToTransmissionAreaCenter = (transform.position - transmissionArea.transform.position).sqrMagnitude;
-
-      return distanceSqrToTransmissionAreaCenter > transmissionArea.MaxBoundsFromTransmissionAreaSqr;
+        if (GameStateController.CanRunGameplay)
+        {
+            UpdateTimer(Time.deltaTime);
+        }
     }
-  }
 
-  /// <summary>
-  /// Returns how much time is left on the timer
-  /// </summary>
-  public float TimeLeft
-  {
-    get
+    /// <summary>
+    /// Finds transmission area in scene and returns true if it is found.
+    /// </summary>
+    /// <returns>True if it can find a TransmissionArea in scene. Else false</returns>
+    private bool CanFindTransmissionArea()
     {
-      if (timer == null) { return -1; }
-      else { return timer.TimeLeft; }
+        transmissionArea = FindObjectOfType<TransmissionArea>();
+        return transmissionArea != null;
     }
-  }
 
-  /// <summary>
-  /// Rising Percentage until timer is completed
-  /// </summary>
-  public float TimePercentage
-  {
-    get
+    /// <summary>
+    /// Set timer to its initial state
+    /// </summary>
+    private void InitTimer()
     {
-      if (timer == null) { return -1; }
-      else { return timer.TimePercentage; }
-    }
-  }
-
-  /// <summary>
-  /// Notifies the listeners that a timer event has triggered.
-  /// Timer events trigger when the player goes out of bounds or enters the bounds again.
-  /// </summary>
-  public TimerEventHandler NotifyTimerEvent
-  {
-    get
-    {
-      if (timer == null)
-      {
-        Debug.LogError("Hooking up an event to a null object");
-        return null;
-      }
-      else
-      {
-        return timer.NotifyTimerEvent;
-      }
-    }
-    set { timer.NotifyTimerEvent = value; }
-  }
-
-  // Start is called before the first frame update
-  void Awake()
-  {
-    // Find TransmissionArea, disable if not found
-    if (!CanFindTransmissionArea())
-    {
-      this.gameObject.SetActive(false);
-      Debug.LogWarning("BoundsChecker did not find a TransmissionArea in scene. Disabling bounds");
-    }
-
-    InitTimer();
-  }
-
-  // Update is called once per frame
-  void Update()
-  {
-    if (GameStateController.CanRunGameplay)
-    {
-      UpdateTimer(Time.deltaTime);
-    }
-  }
-
-  /// <summary>
-  /// Finds transmission area in scene and returns true if it is found.
-  /// </summary>
-  /// <returns>True if it can find a TransmissionArea in scene. Else false</returns>
-  private bool CanFindTransmissionArea()
-  {
-    transmissionArea = FindObjectOfType<TransmissionArea>();
-    return transmissionArea != null;
-  }
-
-  /// <summary>
-  /// Set timer to its initial state
-  /// </summary>
-  private void InitTimer()
-  {
-    timer = new Timer(MAX_TIME);
+        timer = new Timer(MAX_TIME);
 
         PlayerHealth playerHealth = FindObjectOfType<PlayerHealth>();
         if (playerHealth != null) { timer.NotifyTimerComplete += playerHealth.KillPlayer; }
-  }
+    }
 
-  /// <summary>
-  /// Handles timer logic during gametime
-  /// </summary>
-  /// <param name="deltaTime">The amound of time since last frame</param>
-  private void UpdateTimer(float deltaTime)
-  {
-    if (timer.HasStarted)
+    /// <summary>
+    /// Handles timer logic during gametime
+    /// </summary>
+    /// <param name="deltaTime">The amound of time since last frame</param>
+    private void UpdateTimer(float deltaTime)
     {
-      if (!PlayerIsOutsideBounds)
-      {
-        // in bounds
-        Debug.Log("In bounds");
-        timer.TimerReset();
-      }
-      else
-      {
-        // out of bounds
-        timer.Tick(deltaTime);
-      }
+        if (timer.HasStarted)
+        {
+            if (!PlayerIsOutsideBounds)
+            {
+                // in bounds
+                Debug.Log("In bounds");
+                timer.TimerReset();
+            }
+            else
+            {
+                // out of bounds
+                timer.Tick(deltaTime);
+            }
+        }
+        else if (PlayerIsOutsideBounds)
+        {
+            // outside of bounds
+            Debug.Log("Out of bounds");
+            timer.TimerStart();
+        }
     }
-    else if (PlayerIsOutsideBounds)
-    {
-      // outside of bounds
-      Debug.Log("Out of bounds");
-      timer.TimerStart();
-    }
-  }
 }
