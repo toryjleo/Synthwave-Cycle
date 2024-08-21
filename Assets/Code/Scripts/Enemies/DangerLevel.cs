790938bc48d151fa8833ebdb0b134a27bf4e33ed
toryjleo <toryjleo@gmail.com>
2024-08-21 15:48:28 -0400

    set clamp

--- Assets/Code/Scripts/Enemies/DangerLevel.cs
+++ Assets/Code/Scripts/Enemies/DangerLevel.cs
@@ -72,7 +72,7 @@ public class DangerLevel : MonoBehaviour, IResettable
     internal void IncreaseDangerLevel(int dlScore)
     {
         dangerLevel += dlScore;
-        Mathf.Clamp(dangerLevel, minimumThreshold, maximumThreshold);
+        dangerLevel = Mathf.Clamp(dangerLevel, minimumThreshold, maximumThreshold);
     }
 
     public void ResetGameObject()
