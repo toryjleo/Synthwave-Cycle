1f5c1a30786d1d4c3a6aaf8d5de4d728d93addfc
Shadowmage <baconispowerful@gmail.com>
2024-08-23 11:53:04 -0400

    Added noise player for dual radio emitter

--- Assets/Code/Scripts/Audio/DualRadioEmitter.cs
+++ Assets/Code/Scripts/Audio/DualRadioEmitter.cs
@@ -4,31 +4,42 @@ public class DualRadioEmitter : DualAudioEmitter
 {
     private BoundsChecker boundsChecker;
 
+    [SerializeField]
+    private AudioSource noisePlayer;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        if (!noisePlayer)
+        {
+            Debug.LogWarning("No noise player in dual radio emitter!");
+        }
+        else
+        {
+            noisePlayer.mute = true;
+        }
 
         boundsChecker = FindObjectOfType<BoundsChecker>();
-        if (boundsChecker == null) 
+        if (boundsChecker == null)
         {
             Debug.LogWarning("Could not find BoundsChecker");
         }
-        else 
+        else
         {
             boundsChecker.transmissionBoundsEvent += HandleTransmissionBoundsEvent;
         }
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        noisePlayer.volume = 1 - boundsChecker.TransmissionClarity;
     }
 
-    private void HandleTransmissionBoundsEvent(bool isWithinBounds) 
+    private void HandleTransmissionBoundsEvent(bool isWithinBounds)
     {
         Mute(!isWithinBounds);
+        noisePlayer.mute = !isWithinBounds;
     }
 }
