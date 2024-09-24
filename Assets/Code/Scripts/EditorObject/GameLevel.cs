73639348f736c6a9e8be83aa456dbf98e2671977
Shadowmage <baconispowerful@gmail.com>
2024-09-24 14:07:01 -0400

    Namespace for GameLevel EditorObject

--- Assets/Code/Scripts/EditorObject/GameLevel.cs
+++ Assets/Code/Scripts/EditorObject/GameLevel.cs
@@ -1,43 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
-using EditorObject;
 using UnityEngine;
 using UnityEngine.UI;
 
-/// <summary>
-/// Holds information for a specific level in the game, including the ground material and wave sequence
-/// </summary>
-[CreateAssetMenu(menuName = "EditorObject/Game Level", fileName = "New Game Level")]
-public class GameLevel : ScriptableObject
+namespace EditorObject
 {
-    #region Fields
     /// <summary>
-    /// Wave Sequence to be played through
+    /// Holds information for a specific level in the game, including the ground material and wave sequence
     /// </summary>
-    [SerializeField] private WaveSequence waveSequence;
-    /// <summary>
-    /// Material to use for the ground texture
-    /// </summary>
-    [SerializeField] private Material groundMat;
-    /// <summary>
-    /// Name of the level
-    /// </summary>
-    [SerializeField] private string levelName;
-    /// <summary>
-    /// Image to be used in Level Select
-    /// </summary>
-    [SerializeField] private Image levelImage;
-    /// <summary>
-    /// Level cosmetics including particle effects and other visuals
-    /// </summary>
-    [SerializeField] private GameObject[] levelCosmetics;
-    #endregion
+    [CreateAssetMenu(menuName = "EditorObject/Game Level", fileName = "New Game Level")]
+    public class GameLevel : ScriptableObject
+    {
+        #region Fields
+        /// <summary>
+        /// Wave Sequence to be played through
+        /// </summary>
+        [SerializeField] private WaveSequence waveSequence;
+        /// <summary>
+        /// Material to use for the ground texture
+        /// </summary>
+        [SerializeField] private Material groundMat;
+        /// <summary>
+        /// Name of the level
+        /// </summary>
+        [SerializeField] private string levelName;
+        /// <summary>
+        /// Image to be used in Level Select
+        /// </summary>
+        [SerializeField] private Image levelImage;
+        /// <summary>
+        /// Level cosmetics including particle effects and other visuals
+        /// </summary>
+        [SerializeField] private GameObject[] levelCosmetics;
+        #endregion
 
-    #region Properties
-    public WaveSequence WaveSequence { get { return waveSequence; } }
-    public Material GroundMat { get { return groundMat; } }
-    public string LevelName { get { return levelName; } }
-    public Image LevelImage { get { return levelImage; } }
-    public GameObject[] LevelCosmetics { get { return levelCosmetics; } }
-    #endregion
+        #region Properties
+        public WaveSequence WaveSequence { get { return waveSequence; } }
+        public Material GroundMat { get { return groundMat; } }
+        public string LevelName { get { return levelName; } }
+        public Image LevelImage { get { return levelImage; } }
+        public GameObject[] LevelCosmetics { get { return levelCosmetics; } }
+        #endregion
+    }
 }
