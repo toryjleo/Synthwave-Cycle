3b47c10e8885466c34f4f701cf76d63e06cabafd
toryjleo <toryjleo@gmail.com>
2024-12-03 15:59:56 -0500

    Change reference to object

--- Assets/Code/Scripts/Utilities/LevelManager.cs
+++ Assets/Code/Scripts/Utilities/LevelManager.cs
@@ -47,8 +47,8 @@ public class LevelManager : MonoBehaviour
     {
         jukebox.Init(currentLevel.WaveSequence);
         worldGenerator.CreateGround(currentLevel.GroundMat);
-        ImpactManager.Instance.Init();
-        ImpactManager.Instance.AddGroundMapping(currentLevel.GroundImpactMapping);
+        ImpactManager.Instance?.Init();
+        ImpactManager.Instance?.AddGroundMapping(currentLevel.GroundImpactMapping);
         resetObjects = FindObjectsOfType<MonoBehaviour>(true).OfType<IResettable>().ToList();
 
         Arsenal arsenal = FindObjectOfType<Arsenal>();
@@ -75,7 +75,7 @@ public class LevelManager : MonoBehaviour
         Debug.Log("Resetting!");
         resetObjects = FindObjectsOfType<MonoBehaviour>(true).OfType<IResettable>().ToList();
         DangerLevel.Instance.dangerLevel = 0;//make sure we don't start at the same level
-        ImpactManager.Instance.Reset();
+        ImpactManager.Instance?.Reset();
         foreach (IResettable r in resetObjects)
         {
             r.ResetGameObject();
