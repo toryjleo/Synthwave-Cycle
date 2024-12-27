b1dfd537250a1949691c5dd13270d7e6f43b308a
toryjleo <toryjleo@gmail.com>
2024-12-27 11:55:37 -0500

    Moved some properties around

--- Assets/Code/Scripts/EditorObject/GunStats.cs
+++ Assets/Code/Scripts/EditorObject/GunStats.cs
@@ -99,24 +99,7 @@ namespace EditorObject
         [Range(.001f, 400f)][SerializeField] private float accuracyArrivalOverTime = 100;
         #endregion
 
-        #region Overheat
-        /// <summary>
-        /// If the gun can overheat from firing
-        /// </summary>
-        [SerializeField] private bool canOverheat = false;
-        /// <summary>
-        /// The percentage the gun must cool down to before shooting again
-        /// </summary>
-        [Range(0, 100)][SerializeField] private float coolDownPercentageBarrier = 50;
-        /// <summary>
-        /// How much each shot will overheat the gun
-        /// </summary>
-        [Range(1, 100)][SerializeField] private float overHeatPercentPerShot = 5;
-        /// <summary>
-        /// Percentage the gun cools down each second
-        /// </summary>
-        [Range(0, 100)][SerializeField] private float coolDownPerSecond = 2.5f;
-        #endregion
+
 
         #region DEBUG
         /// <summary>
@@ -125,7 +108,7 @@ namespace EditorObject
         [SerializeField] private bool printDebugState = false;
         #endregion
 
-        #region Bullet Specification
+        #region Bullet Options
         /// <summary>
         /// Type of bullet to shoot
         /// </summary>
@@ -159,6 +142,25 @@ namespace EditorObject
         [SerializeField] private float secondsBeforeExplode = 1.0f;
         #endregion
 
+        #region Overheat
+        /// <summary>
+        /// If the gun can overheat from firing
+        /// </summary>
+        [SerializeField] private bool canOverheat = false;
+        /// <summary>
+        /// The percentage the gun must cool down to before shooting again
+        /// </summary>
+        [Range(0, 100)][SerializeField] private float coolDownPercentageBarrier = 50;
+        /// <summary>
+        /// How much each shot will overheat the gun
+        /// </summary>
+        [Range(1, 100)][SerializeField] private float overHeatPercentPerShot = 5;
+        /// <summary>
+        /// Percentage the gun cools down each second
+        /// </summary>
+        [Range(0, 100)][SerializeField] private float coolDownPerSecond = 2.5f;
+        #endregion
+
         #region Area Of Effect
         [Range(0, 1000)][SerializeField] private float damagePerSecond = 10;
         [SerializeField] private Gun.AOEPhases numPhases = Gun.AOEPhases.Persistant;
@@ -170,71 +172,25 @@ namespace EditorObject
         #region Properties
         public bool IsTurret { get { return isTurret; } }
         public bool IsAutomatic { get { return isAutomatic; } }
-        public BulletType BulletType { get { return bulletType; } }
         public bool IsPlayerGun { get { return isPlayerGun; } }
 
-        #region Burst Fire
-        public bool IsBurstFire { get { return shotBurstCount > 1; } }
-        public int ShotBurstCount { get { return shotBurstCount; } }
-        public float TimeBetweenBurstShots { get { return timeBetweenBurstShots; } }
-        public float TimeBetweenShots { get { return timeBetweenShots; } }
-        public float DeltaWindupPercentTimeBetweenShots { get { return deltaWindupPercentTimeBetweenShots / 100f; } }
-        public float DeltaWindDownPercentTimeBetweenShots { get { return deltaWindDownPercentTimeBetweenShots / 100f; } }
-        public float TimeBetweenShotsScalingOverTime { get { return timeBetweenShotsScalingOverTime / 100f; } }
-
-        #endregion
-
-        #region Ammo
-        public bool HasInfiniteAmmo { get { return hasInfiniteAmmo; } }
-        public int AmmoCount { get { return ammoCount; } }
-        #endregion
-
-        #region Multiple Projectiles
-        public int ProjectilesReleasedPerShot { get { return projectilesReleasedPerShot; } }
-        public float AngleBetweenProjectiles { get { return angleBetweenProjectiles; } }
-        public float RandomSpreadPerProjectile { get { return randomSpreadPerProjectile; } }
-        public float DeltaWindUpSpreadPerSecond { get { return deltaWindUpSpreadPerSecond / 100f; } }
-        public float DeltaWindDownSpreadPerSecond { get { return deltaWindDownSpreadPerSecond / 100f; } }
-        public float AccuracyArrivalOverTime { get { return accuracyArrivalOverTime / 100f; } }
-        #endregion
-
-        #region Overheat
-        public bool CanOverheat { get { return canOverheat; } }
-        public float CoolDownPercentageBarrier { get { return coolDownPercentageBarrier; } }
-        public float OverHeatPercentPerShot { get { return overHeatPercentPerShot; } }
-        public float CoolDownPerSecond { get { return coolDownPerSecond; } }
-        #endregion
-
-        #region DEBUG
-        public bool PrintDebugState { get { return printDebugState; } }
-        #endregion
-
-        #region Bullet Specification
+        #region Bullet Options
+        public BulletType BulletType { get { return bulletType; } }
         public float DamageDealt { get { return damageDealt; } }
         public int BulletPenetration { get { return bulletPenetration; } }
         #region Projectile
         public float MuzzleVelocity { get { return muzzleVelocity; } }
         public Vector3 ProjectileScale { get { return projectileScale; } }
         #endregion
+
         #region HitScan
+        public bool IsHitScan { get { return BulletType == EditorObject.BulletType.HitScan; } }
         /// <summary>
         /// Maximum range the projectile travels
         /// </summary>
         public float Range { get { return range; } }
         #endregion
-        public bool IsExplosive { get { return isExplosive; } }
-        public bool IsCountDownExplosion { get { return isCountDownExplosion; } }
-        #endregion
 
-        #region Explosion
-        public float Radius { get { return radius; } }
-        public float Force { get { return force; } }
-        public float ExplosionDamage { get { return explosionDamage; } }
-        public float SecondsBeforeExplode { get { return secondsBeforeExplode; } }
-        #endregion
-
-
-        public bool IsHitScan { get { return BulletType == EditorObject.BulletType.HitScan; } }
         public bool IsBulletProjectile { get { return BulletType == EditorObject.BulletType.BulletProjectile; } }
 
         #region Area of Effect
@@ -243,6 +199,57 @@ namespace EditorObject
         public Gun.AOEPhases NumPhases { get { return numPhases; } }
         public AOEPhase Phase1 { get { return phase1; } }
         public AOEPhase Phase2 { get { return phase2; } }
+        #endregion
+
+        public bool IsCountDownExplosion { get { return isCountDownExplosion; } }
+
+        #region Ammo
+        public bool HasInfiniteAmmo { get { return hasInfiniteAmmo; } }
+        public int AmmoCount { get { return ammoCount; } }
+        #endregion
+
+        #region Multiple Projectiles
+        public int ProjectilesReleasedPerShot { get { return projectilesReleasedPerShot; } }
+        public float RandomSpreadPerProjectile { get { return randomSpreadPerProjectile; } }
+        #endregion
+
+        public bool IsBurstFire { get { return shotBurstCount > 1; } }
+
+        #region Overheat
+        public bool CanOverheat { get { return canOverheat; } }
+        public float CoolDownPercentageBarrier { get { return coolDownPercentageBarrier; } }
+        public float OverHeatPercentPerShot { get { return overHeatPercentPerShot; } }
+        public float CoolDownPerSecond { get { return coolDownPerSecond; } }
+        #endregion
+
+        #region Explosion
+        public bool IsExplosive { get { return isExplosive; } }
+        public float Radius { get { return radius; } }
+        public float Force { get { return force; } }
+        public float ExplosionDamage { get { return explosionDamage; } }
+        public float SecondsBeforeExplode { get { return secondsBeforeExplode; } }
+        #endregion
+        #endregion
+
+
+        #region Accuracy
+        public float AngleBetweenProjectiles { get { return angleBetweenProjectiles; } }
+        public float AccuracyArrivalOverTime { get { return accuracyArrivalOverTime / 100f; } }
+        public float DeltaWindUpSpreadPerSecond { get { return deltaWindUpSpreadPerSecond / 100f; } }
+        public float DeltaWindDownSpreadPerSecond { get { return deltaWindDownSpreadPerSecond / 100f; } }
+        #endregion
+
+        #region Time Between Shots
+        public float TimeBetweenShots { get { return timeBetweenShots; } }
+        public int ShotBurstCount { get { return shotBurstCount; } }
+        public float TimeBetweenBurstShots { get { return timeBetweenBurstShots; } }
+        public float TimeBetweenShotsScalingOverTime { get { return timeBetweenShotsScalingOverTime / 100f; } }
+        public float DeltaWindupPercentTimeBetweenShots { get { return deltaWindupPercentTimeBetweenShots / 100f; } }
+        public float DeltaWindDownPercentTimeBetweenShots { get { return deltaWindDownPercentTimeBetweenShots / 100f; } }
+        #endregion
+
+        #region DEBUG
+        public bool PrintDebugState { get { return printDebugState; } }
         #endregion
 
         #endregion
